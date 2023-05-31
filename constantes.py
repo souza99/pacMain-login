f8e24f3fc04e722f4ec3ba612fa3d24929a5d698
jpfsouza99 <jpfsouza99@gmail.com>
2023-05-30 21:56:58 -0300

    login PacMain

---
+++ constantes.py
@@ -0,0 +1,27 @@
+
+#dimenções da tela
+LARGURA = 480
+ALTURA = 600
+
+#titulo do jogo
+TITULO_JOGO = 'PAC-MAIN'
+
+#FPS
+FPS = 30
+
+#Cores
+PRETO = (0,0,0)
+AMARELO = (224, 223, 51)
+
+#Imagens
+SPRITESHEET = 'spritesht.png'
+PACMAIN_START_LOGO = 'pacman-logo-1.png'
+
+#Fonte
+FONTE = 'arial'
+
+#Cores
+PRETO = (0, 0, 0)
+BRANCO = (255, 255, 255)
+
+TECLA_START = 'munch_1.wav'
